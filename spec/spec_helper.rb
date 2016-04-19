1e7e60bad77dc12c73937f9e553ca6d1097420dd
Colin Young <me@colinyoung.com>
2016-04-19 17:09:21 -0500

    Allow codeclimate report uploads.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :webmock # or :fakeweb
   config.default_cassette_options = { record: :none }
+
+  # For uploading coverage reports
+  config.ignore_hosts 'codeclimate.com'
 end
